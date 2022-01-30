895a48430a33f5c74f5143a4f04914a96aaee5d3
dh5harris <dh5harris@hotmail.com>
2022-01-29 20:02:30 -0600

    Added date

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -1,12 +1,17 @@
 const date = new Date();
-const month = date.getMonth();
-const year = date.getFullYear();
-const day = date.getDate();
-const calenderMonth = ['Janurary', 'Feburary', 'March', '']
-const fullDate = month + ' ' + day + ' ' + year;
+
+const datefield = document.getElementById('today');
+const today = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
+datefield.innerText = today;
 
 
 
 document.getElementById("currentyear").textContent = date.getFullYear();
 document.getElementById('lastupdate').textContent = document.lastModified;
-document.getElementById('today').textContent = fullDate;
+
+
+// Hamburger button
+const hamburgerButton = document.querySelector('.hambugerBtn');
+const miniNav = document.querySelector('.navigation');
+
+hamburgerButton.addEventListener('click', () => {miniNav.classList.toggle('responsive')}, false);
