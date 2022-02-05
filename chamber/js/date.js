a8b7ed015f47388f8f47348bf0db31b138a833c5
dh5harris <dh5harris@hotmail.com>
2022-02-04 23:48:16 -0600

    added content and styles to placeholders

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -5,10 +5,17 @@ const today = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date)
 datefield.innerText = today;
 
 
-
 document.getElementById("currentyear").textContent = date.getFullYear();
 document.getElementById('lastupdate').textContent = document.lastModified;
 
+// Join Banner for Mondays and Tuesdays
+const dayOfWeek = date.getDay();
+const joinMessage = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+
+if (dayOfWeek >= 1 && dayOfWeek <= 2) {
+  document.getElementById('joinBanner').style.display = 'block';
+  document.getElementById('joinBanner').textContent = joinMessage;
+}
 
 // Hamburger button
 const hamburgerButton = document.querySelector('.hambugerBtn');
