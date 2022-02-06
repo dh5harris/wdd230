48eec62d9053dd3a09d7e5591ac98bdc46711456
dh5harris <dh5harris@hotmail.com>
2022-02-05 19:47:41 -0600

    added red x to hamburger btn

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -22,3 +22,4 @@ const hamburgerButton = document.querySelector('.hambugerBtn');
 const miniNav = document.querySelector('.navigation');
 
 hamburgerButton.addEventListener('click', () => {miniNav.classList.toggle('responsive')}, false);
+hamburgerButton.addEventListener('click', () => {hamburgerButton.classList.toggle('responsive')}, false);
