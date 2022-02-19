1a42ea33177a6262e10ec0b6361fb86d7295800f
dh5harris <dh5harris@hotmail.com>
2022-02-19 15:33:45 -0600

    added some comments

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -1,14 +1,17 @@
 const date = new Date();
 
+// Display the date in the header
 const datefield = document.getElementById('today');
 const today = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
 datefield.innerText = today;
 
-
+// Display the copyright year
 document.getElementById("currentyear").textContent = date.getFullYear();
+
+// Display when the last time the document was modified
 document.getElementById('lastupdate').textContent = document.lastModified;
 
-// Join Banner for Mondays and Tuesdays
+// Display the Join Banner for Mondays and Tuesdays
 const dayOfWeek = date.getDay();
 const joinMessage = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
 
