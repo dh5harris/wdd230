af7c97f51823f8fa790b047bfadb3f1eb3edc833
dh5harris <dh5harris@hotmail.com>
2022-02-19 14:56:02 -0600

    added time between visits JS file

---
+++ chamber/js/timebetween.js
@@ -0,0 +1,34 @@
+// Number of days since the user has visted for the discover page
+
+
+let lastVisit = document.querySelector('#lastVisit');
+
+// get the timestamp from the users lastvisit from localStorage
+const lastvisit = Number(localStorage.getItem('lastvisit'));
+
+// math to convert milliseconds to days
+const FACTOR = 1000 * 60 * 60 * 24;//
+
+// Subtract the last visit from the current visit or time
+const currentvisit = Date.now() - lastvisit;
+console.log(FACTOR)
+console.log(typeof(lastVisit))
+console.log(lastvisit)
+console.log(currentvisit)
+// Math to get the number of days
+const daysbetween = currentvisit / FACTOR;
+console.log(daysbetween)
+const firstMessage = `Welcome back! It has been ${Math.round(daysbetween)} days since your last visit.`
+const repeatMessage = 'Welcome, This is your first visit.'
+
+// display to the page how many days since the user's last visit
+if (lastvisit !== 0) {
+  lastVisit.textContent = firstMessage;
+} else {
+  lastVisit.textContent = repeatMessage;
+}
+
+
+
+// set the localStorage with a time stamp to the users current visit to the doscover page
+localStorage.setItem('lastvisit', Date.now())
