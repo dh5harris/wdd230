1211bdc13379af48e21c0b5dd4dfae53a6d34343
dh5harris <dh5harris@hotmail.com>
2022-02-22 18:38:50 -0600

    removed joinBanner from date.js

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -11,14 +11,6 @@ document.getElementById("currentyear").textContent = date.getFullYear();
 // Display when the last time the document was modified
 document.getElementById('lastupdate').textContent = document.lastModified;
 
-// Display the Join Banner for Mondays and Tuesdays
-const dayOfWeek = date.getDay();
-const joinMessage = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
-
-if (dayOfWeek >= 1 && dayOfWeek <= 2) {
-  document.getElementById('joinBanner').style.display = 'block';
-  document.getElementById('joinBanner').textContent = joinMessage;
-}
 
 // Hamburger button
 const hamburgerButton = document.querySelector('.hambugerBtn');
