aba4edde60c795be9dfcb84f0e4c7ff44a9d4e19
dh5harris <dh5harris@hotmail.com>
2022-02-25 20:25:01 -0600

    moved hamburger function to its own file

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -10,11 +10,3 @@ document.getElementById("currentyear").textContent = date.getFullYear();
 
 // Display when the last time the document was modified
 document.getElementById('lastupdate').textContent = document.lastModified;
-
-
-// Hamburger button
-const hamburgerButton = document.querySelector('.hambugerBtn');
-const miniNav = document.querySelector('.navigation');
-
-hamburgerButton.addEventListener('click', () => {miniNav.classList.toggle('responsive')}, false);
-hamburgerButton.addEventListener('click', () => {hamburgerButton.classList.toggle('responsive')}, false);
