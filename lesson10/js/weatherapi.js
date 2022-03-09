c845d3cd0ee1f28e9e3e60f07dae3bfc50c205ac
dh5harris <dh5harris@hotmail.com>
2022-03-09 14:37:53 -0600

    some changes

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,8 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&units=imperial&APPID=f66666e34de75c8542449d7d4931f7b6'
+const part1 = 'f66666e34de';
+const part2 = '75c8542449';
+const part3 = 'd7d4931f7b6';
 
-
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&units=imperial&APPID=${part1}${part2}${part3}`
 
 
 fetch(apiURL)
