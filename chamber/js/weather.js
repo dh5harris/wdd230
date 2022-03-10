794e37e7bf3f2eccdaa81238d85598ae42fe3320
dh5harris <dh5harris@hotmail.com>
2022-03-10 00:14:15 -0600

    added windchill function

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,18 +1,43 @@
-const part1 = 'f66666e34de';
-const part2 = '75c8542449';
-const part3 = 'd7d4931f7b6';
+const info = 'f66666e34de75c8542449d7d4931f7b6';
 
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Paducah,ky,us&units=imperial&APPID=${part1}${part2}${part3}`
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Paducah,ky,us&units=imperial&APPID=${info}`
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     // console.log(jsObject);
-    document.querySelector('#temperature').textContent = (jsObject.main.temp).toFixed(1);
+
+    // set variables from the html elements
+    const icon = document.querySelector('#icon');
+    const temp = document.querySelector('#temperature');
+    const wind = document.querySelector('#windSpeed');
+    const description = document.querySelector('#description');
+
+    // set variables to a value
+    const currentTemp = jsObject.main.temp;
+    const windSpeed = jsObject.wind.speed;
     const weatherIcon = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const weatherDescription = jsObject.weather[0].description;
-    document.querySelector('#icon').setAttribute('src', weatherIcon);
-    document.querySelector('#icon').setAttribute('alt', weatherDescription);
-    document.querySelector('#description').textContent = weatherDescription;
-    document.querySelector('#windSpeed').textContent = (jsObject.wind.speed).toFixed();
+
+    // set html elements to the values
+    temp.textContent = currentTemp.toFixed(0);
+    icon.setAttribute('src', weatherIcon);
+    icon.setAttribute('alt', weatherDescription);
+    description.textContent = weatherDescription;
+    wind.textContent = windSpeed.toFixed();
+
+    // To check for the windchill
+    if (currentTemp <= 50 && windSpeed > 3) {
+      windchill = windChill(currentTemp, windSpeed);
+      windchill = `${windchill}&#176;F`;
+    } else {
+      windchill = 'N/A';
+    }
+
+    document.querySelector('#windChill').innerHTML = windchill;
+
+    // The formula to calculate the wind chill factor. The .toFixed is to round the number.
+    function windChill (temp, speed) {
+      return (35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16)).toFixed();
+}
   })
