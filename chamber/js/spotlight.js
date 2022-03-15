e607ff2093749b05bf6683fa2f38336031e7ad2e
dh5harris <dh5harris@hotmail.com>
2022-03-15 14:26:05 -0500

    added spotlight to pull from json file

---
+++ chamber/js/spotlight.js
@@ -0,0 +1,59 @@
+const resquestURL = 'https://dh5harris.github.io/wdd230/chamber/data/data.json';
+const spotlightBoxMain = document.querySelector('.spotlightBoxMain');
+
+fetch(resquestURL)
+  .then(function (repsone){
+    return repsone.json();
+  })
+  .then(function (jsonObject) {
+    console.log(jsonObject)
+    const businesses = jsonObject['businesses'];
+    let gold = businesses.filter(businesses => (businesses.membershiplevel === 'gold'));
+    for (i=0; i<3; i++) {
+      x = Math.floor(Math.random() * gold.length)
+      displaySpotight(gold[x], `spot${i+1}`);
+      gold = gold.filter(business => (business.name != gold[x].name));
+    }
+
+    // businesses.forEach(displaySpotight);
+  });
+
+  function displaySpotight(business, spot) {
+    // Create elements for spotlight boxes
+    console.log(spot)
+    let spotlightBox = document.createElement('div');
+    spotlightBox.classList.add('placeholderBox', 'spotlightBox', `${spot}`)
+    let spotlightSection = document.createElement('section');
+    let businessName = document.createElement('h3');
+    let logo = document.createElement('img');
+    let motto = document.createElement('p');
+    let info = document.createElement('div');
+    info.classList.add('info');
+    let email = document.createElement('p');
+    let phone =  document.createElement('p');
+
+    // Add textContent property to fill spotlight boxes
+    businessName.textContent = `${business.businessname}`;
+    motto.textContent = `${business.motto}`;
+    console.log(business.motto)
+    email.textContent = `${business.email}`;
+    phone.textContent = `Phone: ${business.cellphone}`;
+
+    // Set img attributes for logos
+    logo.setAttribute('src', business.imageurl);
+    logo.setAttribute('alt', `Logo of ${business.businessName}`)
+
+    // Add business information to the spotlight section
+    spotlightSection.appendChild(businessName);
+    spotlightSection.appendChild(logo);
+    spotlightSection.appendChild(motto);
+    info.appendChild(email);
+    info.appendChild(phone);
+
+    // Add the spotlightBoxMain div 
+    spotlightBox.appendChild(spotlightSection);
+    spotlightBox.appendChild(info);
+
+    spotlightBoxMain.appendChild(spotlightBox)
+
+  }
