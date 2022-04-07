315544e39075ce8cf1911e5c80472dddf4480e3e
dh5harris <dh5harris@hotmail.com>
2022-04-06 21:54:34 -0500

    tried to add like to local storgae

--- temple/js/temples.js
+++ temple/js/temples.js
@@ -8,65 +8,63 @@ fetch(requestURL)
   .then(function (jsObject) {
     const temples = jsObject['temples'];
     temples.forEach(dispalyTemples);
+
   });
 
-  function dispalyTemples(temples) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    card.classList.add('spot', 'pad');
-    let templeName = document.createElement('h3');
-    templeName.classList.add('header')
-    let templeImage = document.createElement('img');
-    let address = document.createElement('p');
-    let phone = document.createElement('p');
-    let services = document.createElement('p');
-    let servicesList = document.createElement('ul');
-    let history = document.createElement('p');
-    let historyList = document.createElement('ul');
-    let likeBtn = document.createElement('button');
-    likeBtn.classList.add('btn', 'like')
+function dispalyTemples(temples) {
+  // Create elements to add to the document
+  let card = document.createElement('section');
+  card.classList.add('spot', 'pad');
+  let templeName = document.createElement('h3');
+  templeName.classList.add('header')
+  let templeImage = document.createElement('img');
+  let address = document.createElement('p');
+  let phone = document.createElement('p');
+  let services = document.createElement('p');
+  let servicesList = document.createElement('ul');
+  let history = document.createElement('p');
+  let historyList = document.createElement('ul');
+  let likeBtn = document.createElement('button');
+  likeBtn.classList.add('btn', 'like')
 
 
-    // Add the textContent property to fill the temple cards
-    templeName.textContent = `${temples.name}`;
-    address.textContent = `Address: ${temples.address}, ${temples.city}, ${temples.state}`;
-    phone.textContent = `Phone: ${temples.phone}`;
-    services.textContent = `Services offered:`;
-    servicesList.innerHTML = `<li>${temples.clothing}</li><li>${temples.housing}</li><li>${temples.distribution}</li>`;
-    history.textContent = `Some temple history:`;
-    historyList.innerHTML = `<li>Announced - ${temples.announced}</li><li>Groundbreaking - ${temples.groundbreaking}</li><li>Dedicated - ${temples.dedicated}</li>`;
-    likeBtn.textContent = `Like`;
+  // Add the textContent property to fill the temple cards
+  templeName.textContent = `${temples.name}`;
+  address.textContent = `Address: ${temples.address}, ${temples.city}, ${temples.state}`;
+  phone.textContent = `Phone: ${temples.phone}`;
+  services.textContent = `Services offered:`;
+  servicesList.innerHTML = `<li>${temples.clothing}</li><li>${temples.housing}</li><li>${temples.distribution}</li>`;
+  history.textContent = `Some temple history:`;
+  historyList.innerHTML = `<li>Announced - ${temples.announced}</li><li>Groundbreaking - ${temples.groundbreaking}</li><li>Dedicated - ${temples.dedicated}</li>`;
+  likeBtn.textContent = `Like`;
 
-    // set the img attributes for the images
-    templeImage.setAttribute('src', temples.imageurl);
-    templeImage.setAttribute('alt', `Image of the ${temples.name}`);
-    templeImage.setAttribute('loading', 'lazy');
+  // set the img attributes for the images
+  templeImage.setAttribute('src', temples.imageurl);
+  templeImage.setAttribute('alt', `Image of the ${temples.name}`);
+  templeImage.setAttribute('loading', 'lazy');
 
-    // add the temple information to the temple section(card)
-    card.appendChild(templeName);
-    card.appendChild(templeImage);
-    card.appendChild(address);
-    card.appendChild(phone);
-    card.appendChild(services);
-    card.appendChild(servicesList);
-    card.appendChild(history);
-    card.appendChild(historyList);
-    card.appendChild(likeBtn);
+  // add the temple information to the temple section(card)
+  card.appendChild(templeName);
+  card.appendChild(templeImage);
+  card.appendChild(address);
+  card.appendChild(phone);
+  card.appendChild(services);
+  card.appendChild(servicesList);
+  card.appendChild(history);
+  card.appendChild(historyList);
+  card.appendChild(likeBtn);
 
-    // add cards to the templeCards div 
-    templeCards.appendChild(card);
-    
-    likeBtn.addEventListener('click', () => {likeBtn.classList.toggle('liked')}, false);
-    // localStorage.setItem('liked');
-    
-  }
+  // add cards to the templeCards div 
+  templeCards.appendChild(card);
 
-  function likedButton() {
-    let likedBtn = document.querySelector('liked');
-    // if 
-    localStorage.setItem('liked');
-  }
-
-
+  likeBtn.addEventListener('click', () => { 
+    if (window.localStorage.getItem('liked')) {
+      likeBtn.classList.toggle('liked')}
+    else {
+      window.localStorage.setItem('liked', 'on');
+      likeBtn.classList.toggle('liked')
+    }
+    })
+}
 
 
